d94d2f393f8bb5ef6eeeb466018d1dff4782b936
Eddie Joseph <eddie.joseph@windowslive.com>
2019-04-14 20:16:27 +0200

    added authentication for group generation

--- src/main/java/ch/eddjos/qualitool/goups/GroupController.java
+++ src/main/java/ch/eddjos/qualitool/goups/GroupController.java
@@ -1,10 +1,12 @@
 package ch.eddjos.qualitool.goups;
 
+import ch.eddjos.qualitool.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.AuthenticationException;
 import java.util.List;
 
 @RestController
@@ -12,17 +14,34 @@ import java.util.List;
 public class GroupController {
     @Autowired
     GroupService service;
+    @Autowired
+    AuthService autService;
     @GetMapping
-    public ResponseEntity<List<Group>> getAll(){
+    public ResponseEntity<List<Group>> getAll(@PathVariable(value = "token",required = false) String token ){
+        try {
+            autService.getAuthentication(token);
+        }catch(AuthenticationException e){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
         return new ResponseEntity(service.findAll(),HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity<Group> create(@RequestBody GroupDTO dto){
+    public ResponseEntity<Group> create(@RequestBody GroupDTO dto, @PathVariable(value = "token",required = false) String token ){
+        try {
+            autService.getAuthentication(token);
+        }catch(AuthenticationException e){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
         return new ResponseEntity(service.insert(dto),HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity delete(@PathVariable("id")int id){
+    public ResponseEntity delete(@PathVariable("id")int id, @PathVariable(value = "token",required = false) String token ){
+        try {
+            autService.getAuthentication(token);
+        }catch(AuthenticationException e){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
         service.delete(id);
         return new ResponseEntity(HttpStatus.OK);
     }
